d09bc0b6cb14a01fb9a63f72b9524265e630e411
ZacharyGoolman <zachary.goolman@gmail.com>
2022-04-25 17:18:22 -0400

    Added POST in Postman to add music to database

--- music_library/views.py
+++ music_library/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
-from rest_framework.response import Response
+from pydoc import resolve
+from urllib import response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import SongSerializer
+from .models import Song
+from rest_framework import status
 
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def songs_list(request):
-    return Response('ok')
+
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response (serializer.data, status=status.HTTP_201_CREATED)
+
+# @api_view(['GET',])          
+     
